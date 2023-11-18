659702b685a2eee67d35fa4d91a42b5f7d6f503c
Husen Hamze <summer.00.projects@gmail.com>
2023-11-18 22:33:57 +0330

    Update color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,33 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        cyan: "hsl(180, 66%, 49%)",
-        cyanLight: "hsl(180, 66%, 69%)",
-        darkViolet: "hsl(257, 27%, 26%)",
-        red: "hsl(0, 87%, 67%)",
-        grayishViolet: "hsl(257, 7%, 63%)",
-        veryDarkBlue: "hsl(255, 11%, 22%)",
-        veryDarkViolet: "hsl(260, 8%, 14%) ",
+        primary: {
+          light: "hsl(180, 66%, 69%)", // Light Cyan
+          main: "hsl(180, 66%, 49%)", // Main Cyan
+          dark: "hsl(180, 66%, 29%)", // Dark Cyan
+        },
+        secondary: {
+          light: "hsl(257, 7%, 73%)", // Light Grayish Violet
+          main: "hsl(257, 7%, 53%)", // Main Grayish Violet
+          dark: "hsl(257, 7%, 33%)", // Dark Grayish Violet
+        },
+        accent: {
+          light: "hsl(0, 87%, 77%)", // Light Red
+          main: "hsl(0, 87%, 57%)", // Main Red
+          dark: "hsl(0, 87%, 37%)", // Dark Red
+        },
+        background: {
+          light: "hsl(0, 0%, 100%)", // Light Background
+          main: "hsl(260, 8%, 14%)", // Main Very Dark Violet
+          dark: "hsl(260, 8%, 10%)", // Dark Very Dark Violet
+        },
+        text: {
+          light: "hsl(0, 0%, 0%)", // Dark Text on Light Background
+          main: "hsl(0, 0%, 100%)", // Light Text on Dark Background
+          dark: "hsl(0, 0%, 80%)", // Gray Text on Dark Background
+        },
       },
+      
       fontFamily: {
         sans: ["Poppins", "sans-serif"],
       },
